db0a3ed9e5e270ece7535c588d7ad722f2c1de6b
Ryo Vamirion <rynortheast@yandex.ru>
2022-07-16 07:01:22 +0300

    Разработано задание #5 - removeReps

--- FRONT_00_0/removeReps.js
+++ FRONT_00_0/removeReps.js
@@ -1,6 +1,13 @@
-// Вам нужно написать функцию которая принимает в кач-ве аргумента массива чисел и удаляет все повторяющиеся значения
 
-function removeReps(array) {}
+function removeReps(array) {
+    for (let x = 0; x < array.length; x += 1) {
+        let y = array.length;
+        while (x != y)
+            array[x] == array[y] ? array.splice(y--, 1) : y--;
+    }
+    return array;
+}
 
-removeReps([1, 1, 2, 4, 5, 6, 6, 8, 9, 11]); // [1,2,4,5,6,8,9,11]
+let result = removeReps([1, 1, 2, 4, 5, 6, 6, 8, 9, 11]); // [1,2,4,5,6,8,9,11]
 
+// console.log(`Результат: ${result}`);
