a7171964420555c22ce1a734ff60571c26857194
Ryo Vamirion <rynortheast@yandex.ru>
2022-07-16 14:57:33 +0300

    Разработано задание #8 - wordSearch

--- FRONT_00_0/wordSearch.js
+++ FRONT_00_0/wordSearch.js
@@ -1,14 +1,30 @@
 
 function searchSubString(puzzle, word) {
     let result = false;
-    if (word.length > 0) {
-        for (let y = 0; y < puzzle.length; y += 1) {
-            for (let x = 0; x < puzzle[y].length; x += 1) {
-    
-                puzzle[y][x] == word[0] ? 1 : 0;
-    
+    if (word.length > 1) {
+        for (let y = 0; y < puzzle.length && result == false; y += 1) {
+            for (let x = 0; x < puzzle[y].length && result == false; x += 1) {
+                if (puzzle[y][x] == word[0]) {
+                    for (let n = -1; n <= 1 && result == false; n += 1) {
+                        for (let m = -1; m <= 1 && result == false; m += 1)
+                            n == 0 == m ? 0 : result = checkWord(puzzle, word, y, x, [n, m]);
+                    }
+                }
             }
         }
+    }
+    return result;
+}
+
+function checkWord(puzzle, word, y, x, indent) {
+    let result = false, n = y, m = x;
+    for (let z = 1; z < word.length && result == false; z += 1) {
+        n += indent[0], m += indent[1];
+        n < 0 ? n = null : n >= puzzle.length ? n = null : 0;
+        m < 0 ? m = null : m >= puzzle[y].length ? m = null : 0;
+        if ((n == null || m == null) || (puzzle[n][m] != word[z]))
+            break;
+        z == (word.length - 1) ? result = true : 0;
     }
     return result;
 }
@@ -24,9 +40,11 @@ const examplePuzzle = [
 ];
 
 // Level 1
-searchSubString(examplePuzzle, "like");   // true
-searchSubString(examplePuzzle, "gold");   // true
-searchSubString(examplePuzzle, "queen");  // true
+let result = searchSubString(examplePuzzle, "like");    // true
+result = searchSubString(examplePuzzle, "gold");        // true
+result = searchSubString(examplePuzzle, "queen");       // true
 
 // Level 2
-searchSubString(examplePuzzle, "cake");   // true
+result = searchSubString(examplePuzzle, "cake");        // true
+
+// console.log(`Результат: ${result}`);
