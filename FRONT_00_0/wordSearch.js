395dfe98743bc06107d32c836f132e92763dede7
Ryo Vamirion <rynortheast@yandex.ru>
2022-07-16 13:46:39 +0300

    Добавлена начальная структура к заданию #8 - wordSearch

--- FRONT_00_0/wordSearch.js
+++ FRONT_00_0/wordSearch.js
@@ -1,26 +1,32 @@
-//  В этой задаче нужно будет написать алгоритм поиска, который скажет, можно ли найти входное слово в головоломке поиска слов, которая тоже подается функции на вход.
-// Данная задача имеет два уровня сложности :
-// - Первый уровень включает в себя исключительно поиск по вертикали и по горизонтали
-// - Второй уровень дополнительно включает в себя поиск по диагонали
-// - Слова могут быть записаны слева направо и наоборот.
 
-function searchSubString(puzzle, word) {}
+function searchSubString(puzzle, word) {
+    let result = false;
+    if (word.length > 0) {
+        for (let y = 0; y < puzzle.length; y += 1) {
+            for (let x = 0; x < puzzle[y].length; x += 1) {
+    
+                puzzle[y][x] == word[0] ? 1 : 0;
+    
+            }
+        }
+    }
+    return result;
+}
 
 const examplePuzzle = [
-  ["b", "l", "g", "o", "l", "d", "s"],
-  ["x", "k", "q", "w", "i", "j", "p"],
-  ["a", "n", "w", "k", "k", "p", "n"],
-  ["h", "e", "e", "e", "k", "i", "l"],
-  ["q", "e", "k", "a", "y", "q", "a"],
-  ["h", "u", "h", "a", "e", "a", "u"],
-  ["k", "q", "j", "c", "c", "m", "r"],
+    ["b", "l", "g", "o", "l", "d", "s"],
+    ["x", "k", "q", "w", "i", "j", "p"],
+    ["a", "n", "w", "k", "k", "p", "n"],
+    ["h", "e", "e", "e", "k", "i", "l"],
+    ["q", "e", "k", "a", "y", "q", "a"],
+    ["h", "u", "h", "a", "e", "a", "u"],
+    ["k", "q", "j", "c", "c", "m", "r"],
 ];
 
 // Level 1
-searchSubString(examplePuzzle, "like"); // true
-searchSubString(examplePuzzle, "gold"); // true
-searchSubString(examplePuzzle, "queen"); // true
+searchSubString(examplePuzzle, "like");   // true
+searchSubString(examplePuzzle, "gold");   // true
+searchSubString(examplePuzzle, "queen");  // true
 
 // Level 2
-searchSubString(examplePuzzle, "cake"); // true
-
+searchSubString(examplePuzzle, "cake");   // true
