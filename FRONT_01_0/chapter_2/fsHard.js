6f4a76eca02cd4a04393eb25c0da840704883000
Ryo Vamirion <rynortheast@yandex.ru>
2022-07-17 15:11:17 +0300

    Разработано чуть-чуть задание #4 - fsHard

--- FRONT_01_0/chapter_2/fsHard.js
+++ FRONT_01_0/chapter_2/fsHard.js
@@ -1,8 +1,15 @@
 
-const fs = require("fs");
+import fs from "fs";
 
 export const progressbar = async () => {
-
+    let files = fs.readdirSync("./files/fsHard");
+    let sumFiles = files.reduce((sum, file) => {
+        let stats = fs.statSync("./files/fsHard/" + file);
+        return sum += (stats.size / 1024);
+    }, 0)
+    console.log("PROGRESS: [ " + "|".repeat(100) + ` ] 100%`);
+    return Math.round(sumFiles);
 }
 
-
+let activeProgressBar = progressbar();
+console.log(await activeProgressBar);
