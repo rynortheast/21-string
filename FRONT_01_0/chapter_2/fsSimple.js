0011ea3c7f8aab3ffbedbbbc35a623100006f36b
Ryo Vamirion <rynortheast@yandex.ru>
2022-07-17 14:48:39 +0300

    Разработано задание #3 - fsSimple

--- FRONT_01_0/chapter_2/fsSimple.js
+++ FRONT_01_0/chapter_2/fsSimple.js
@@ -1,13 +1,50 @@
-const fs = requere("fs");
 
-const readAndWriteCallbackHell = () => {};
+import fs from 'fs';
 
-const readAndWritePromises = () => {};
+const readAndWriteCallbackHell = (callback) => {
+    let data = fs.readFile("./files/fsSimple/file1.txt", (error, data) => {
+        error == null ? fs.writeFile("./files/fsSimple/file2.txt", data, 
+        (error) => {callback(error)}) : callback(error);
+    });
+};
 
-const readAndWriteAsyncAwait = async () => {};
+const readAndWritePromises = () => {
+    let promise = new Promise((resolve, reject) => {
+        let data = fs.readFile("./files/fsSimple/file1.txt", (error, data) => {
+            error == null ? fs.writeFile("./files/fsSimple/file2.txt", data, 
+            (error) => {error == null ? resolve() : reject()}) : reject();
+        });
+    })
+    return promise;
+};
+
+const readAndWriteAsyncAwait = async () => {
+    let result = "3 : Файл file2.txt успешно обновлен!!";
+    const saveResult = (data) => {result = data};
+    let data = fs.readFile("./files/fsSimple/file1.txt", (error, data) => {
+        error == null ? fs.writeFile("./files/fsSimple/file2.txt", data, 
+        (error) => {error == null ? saveResult("3 : Файл file2.txt успешно обновлен!!") : 0}) : 0;
+    });
+    return result;
+};
 
 export {
-  readAndWriteAsyncAwait,
-  readAndWritePromises,
-  readAndWriteCallbackHell,
+    readAndWriteAsyncAwait,
+    readAndWritePromises,
+    readAndWriteCallbackHell,
 };
+
+readAndWriteCallbackHell((error) => {
+    error == null ? console.log(`1 : Файл file2.txt успешно обновлен!!`) :
+    console.log("1 : Произошла некоторая ошибка при . . . файла!!");
+});
+
+let saveData = readAndWritePromises();
+saveData.then(() => {
+    console.log("2 : Файл file2.txt успешно обновлен!!");
+}, () => {
+    console.log("2 : Произошла некоторая ошибка при . . . файла!!");
+})
+
+let asyncData = readAndWriteAsyncAwait();
+console.log(await asyncData);
