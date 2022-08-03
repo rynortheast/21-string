9c9e8da8e3a59bdca3e1a8bfbc68b1a5736ac15a
Ryo Vamirion <rynortheast@yandex.ru>
2022-08-03 23:47:33 +0300

    Удаление FRONTEND проекта (перенес в другой репозиторий)

--- FRONTEND/FRONT_00_0/fibonachi.js
+++
@@ -1,8 +0,0 @@
-
-function fibo(index) {
-    return index < 0 ? 0 : index == 0 ? 1 : fibo(index - 2) + fibo(index - 1);
-}
-
-let result = fibo(21);  //  Вернет - 17711
-
-// console.log(`Результат: ${result}`);
